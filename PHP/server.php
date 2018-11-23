5eb7e54674746757c2aabcac7cfe73f368e7c6c4
David Sheehy <sheehy_d@ymail.com>
2018-11-23 19:28:02 +0000

    insert user query not fixed yet, insert team query working, to be figured out

--- PHP/server.php
+++ PHP/server.php
@@ -41,6 +41,8 @@ if (isset($_POST['reg_user'])) {
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
 
+
+
     if ($user) { // if user exists
         if ($user['username'] === $username) {
             array_push($errors, "Username already exists");
@@ -50,22 +52,32 @@ if (isset($_POST['reg_user'])) {
           //  array_push($errors, "email already exists");
         //}
     }
+    $team_check_query = "SELECT TeamName FROM teams WHERE TeamName='$team'";
+    $teamcheck = mysqli_query($db, $team_check_query);
+    $teamcheckstring = mysqli_fetch_assoc($teamcheck);
+
+    if ($teamcheckstring) {
+        if($teamcheckstring['TeamName'] === $team) {
+            array_push($errors, "Team name already taken");
+        }
+    }
 
     // register user if there are no errors in the form
     if (count($errors) == 0) {
         $password = md5($password_1);//encrypt the password before saving in the database
 
 
+        //inserts a users team into teams entity
         $queryteam = "INSERT INTO teams (TeamName) VALUES ('$team') ";
-        $queryteam2 = "SELECT TeamID FROM teams WHERE TeamName='$team'";
-
         mysqli_query($db, $queryteam);
 
-
+        //return that teams id in teams entity for use in inserting new user into users table
+        $queryteam2 = "SELECT TeamID FROM teams WHERE TeamName='$team'";
         $teamres =  mysqli_query($db, $queryteam2);
         $teamID = mysqli_fetch_assoc($teamres)['TeamID'];
-        echo mysqli_fetch_assoc($teamres)['TeamID'];
 
+
+        //insert user row into users entity !!!!!Something wrong with query here!!!!!!!
         $query = "INSERT INTO users (username, pword, score, TeamID) 
   			  VALUES('$username', '$password',100, $teamID)";
         mysqli_query($db, $query);
@@ -92,7 +104,7 @@ if (isset($_POST['login_user'])) {
         if (mysqli_num_rows($results) == 1) {
             $_SESSION['username'] = $username;
             $_SESSION['success'] = "You are now logged in";
-            header('location: index.php');
+            header('location: ../index.php');
         }else {
             array_push($errors, "Wrong username/password combination");
         }
