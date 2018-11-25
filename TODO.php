4b939a13984a12f22c7812375665a766a558615a
David Sheehy <sheehy_d@ymail.com>
2018-11-25 13:01:51 +0000

    TODO php page all team members look at this. Targets for end of week

---
+++ TODO.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Davy Sheehy
+ * Date: 25/11/2018
+ * Time: 12:49
+ */
+
+/*
+ *  TODO Create Functions as follows:
+ *
+ * Make final changes to Database on players, league_tables, team_rosters, leagues
+ *
+ * Game:
+ * Create function that plays two teams together
+ * Allows team to play others on the league
+ * Once a team has played all other teams, mark done as true
+ * Once all teams are marked done as true, 1st place team gets 100 points, 2nd place team gets 500, 3rd 250 to be added to user account
+ *
+ * SQL Methods to be used for this: SELECT, UPDATE, INSERT
+ *
+ * Create a League with user defined as admin
+ *
+ *
+ *
+ *
+ * Team Management:
+ * Add player to team (from roster)
+ * Remove player from team
+ * Display List of users to choose to add to League
+ *
+ * SQL Methods: SELECT, INSERT, UPDATE, DELETE
+ *
+ * Market:
+ * Sell Player to add points to user
+ * Buy player to add player to roster and deduct user points
+ *
+ * SQL Methods: SELECT, INSERT, UPDATE, DELETE
+ *
+ * Twitter:
+ * Newsfeed on homepage
+ * function to count positive/negative tweets, create a ratio, then add that to equation which calculates rating
+ *
+ * AJAX to display player info on hover of card
+ *
+ * Provide backend to all pages
+ */
