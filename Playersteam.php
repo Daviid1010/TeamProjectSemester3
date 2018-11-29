915e56d12acf9b36381c8e506d696bf9d1b71381
David Sheehy <sheehy_d@ymail.com>
2018-11-29 10:51:18 +0000

    created a connection php file that you can include to your local instance of mysql, if that doesnt work you can generate one using mysql workbench, also created a php function which outputs a team of a user based on the teamID asossiated with a user.

---
+++ Playersteam.php
@@ -0,0 +1,45 @@
+<?php include 'connection.php'?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Davy Sheehy
+ * Date: 29/11/2018
+ * Time: 10:33
+ */
+
+$teamID = 1;
+
+$query = "SELECT playerName, PlayerPosition, Province, Rating FROM team_rosters JOIN players ON players.PlayerID = team_rosters.PlayerID WHERE team_rosters.TeamID=$teamID";
+
+
+if ($stmt = $con->prepare($query)) {
+    $stmt->execute();
+    $stmt->bind_result($playerName, $PlayerPosition, $Province, $Rating);
+
+    echo "<table class=\"table table-hover\">
+                <thead>
+                <tr>
+                    <th scope=\"col\">Name</th>
+                    <th scope=\"col\">Position</th>
+                    <th scope=\"col\">Province</th>
+                    <th scope=\"col\">Rating</th>
+
+                </tr>
+                </thead>
+                <tbody>";
+
+    while ($stmt->fetch()) {
+        echo "<tr>
+                    <td>$playerName</td>
+                    <td>$PlayerPosition</td>
+                    <td>$Province</td>
+                    <td>$Rating</td>
+                </tr>";
+    }
+    echo "</tbody></table>";
+    $stmt->close();
+}
+
+
+
+?>
