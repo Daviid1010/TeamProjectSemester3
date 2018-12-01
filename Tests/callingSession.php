0165434caa32eefe231222fa6e56a4570a509f7a
David Sheehy <sheehy_d@ymail.com>
2018-12-01 15:56:03 +0000

    testing php session variables

---
+++ Tests/callingSession.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Davy Sheehy
+ * Date: 01/12/2018
+ * Time: 15:52
+ */
+include 'sessiontest.php';
+echo "Name: ".$_SESSION["user"]."<br>";
+echo "TeamID: ".$_SESSION["teamID"]."<br>";
+
+?>
---
+++ Tests/sessiontest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Davy Sheehy
+ * Date: 01/12/2018
+ * Time: 15:51
+ */
+session_start();
+
+$_SESSION["user"] = "David";
+$_SESSION["teamID"] = "1";
+
+?>
