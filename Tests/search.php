77cabd5d9e5b93c50fe7aea5387aef19fbcee07b
David Sheehy <sheehy_d@ymail.com>
2018-12-02 20:35:06 +0000

    minor aesthetic changes to search.php

--- Tests/search.php
+++ Tests/search.php
@@ -9,6 +9,9 @@
 <body>
 <div class="navbar-static-top navbar-inverse" id="home">
     <div class="container">
+        <a class="navbar-brand" href="#">
+            <img id="brand-image" src="../images/rugbylogo.png" alt="Irish Fantasy Rugby">
+        </a>
         <div class="navbar-brand">
             Market
         </div>
@@ -24,74 +27,74 @@
         </div>
     </div>
 </div>
-
-<div class="jumbotron">
-    <div class="container">
-        <h1>Player Market</h1>
+<div class="container">
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Player Market</h1>
+        </div>
     </div>
-</div>
 
-<div class="col-md-12">
-    <form method='post' action='search.php'>
-        <label>Search by Name:  </label><input type='text' name='name' />
-        <label> or Province:  </label><input type='text' name='province'/>
-        <label> or Position:  </label><input type='text' name='position' />
-        <input id="submitBtn" type='submit' value='Submit' name='submit'>
-    </form>
+    <div class="col-md-12">
+        <form method='post' action='search.php'>
+            <label>Search by Name:  </label><input type='text' name='name' />
+            <label> or Province:  </label><input type='text' name='province'/>
+            <label> or Position:  </label><input type='text' name='position' />
+            <input id="submitBtn" type='submit' value='Submit' name='submit'>
+        </form>
 
-    <?php
-/**
- * Created by PhpStorm.
- * User: Davy Sheehy
- * Date: 02/12/2018
- * Time: 12:27
- *
- *
- */
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: Davy Sheehy
+         * Date: 02/12/2018
+         * Time: 12:27
+         *
+         *
+         */
 
-include '../connection.php';
-if(isset($_POST['submit'])){
-    $playerName =  $_POST['name'];
-    $province = $_POST['province'];
-    $position = $_POST['position'];
-
+        include '../connection.php';
+        if(isset($_POST['submit'])){
+            $playerName =  $_POST['name'];
+            $province = $_POST['province'];
+            $position = $_POST['position'];
 
 
-    $search = "SELECT PlayerName, PlayerPosition, Province, Rating FROM players";
+
+            $search = "SELECT PlayerName, PlayerPosition, Province, Rating FROM players";
 
 
-    //checks if any of the feilds have been filled
-    if($playerName!=null || $position!=null || $province!=null) {
-        $search .= " WHERE ";
+            //checks if any of the feilds have been filled
+            if($playerName!=null || $position!=null || $province!=null) {
+                $search .= " WHERE ";
 
-        //if player name is not empty and if the twn other are empty
-        if( $playerName!=null && $province==null && $position==null ){
-            $search .= "PlayerName=\"$playerName\"";
-        } else if ($playerName!=null) {
-            $search .= "PlayerName=\"$playerName\" AND ";
-        }
+                //if player name is not empty and if the twn other are empty
+                if( $playerName!=null && $province==null && $position==null ){
+                    $search .= "PlayerName=\"$playerName\"";
+                } else if ($playerName!=null) {
+                    $search .= "PlayerName=\"$playerName\" AND ";
+                }
 
-        //now checking if province is empty and also position
-        if( $province!=null && $position==null) {
-            $search .= "Province=\"$province\"";
-        } else if ($province!=null) {
-            $search .= "Province=\"$province\" AND ";
-        }
+                //now checking if province is empty and also position
+                if( $province!=null && $position==null) {
+                    $search .= "Province=\"$province\"";
+                } else if ($province!=null) {
+                    $search .= "Province=\"$province\" AND ";
+                }
 
-        //finally check to see if postion is full
-        if ($position!=null) {
-            $search .= "PlayerPosition=\"$position\"";
-        }
+                //finally check to see if postion is full
+                if ($position!=null) {
+                    $search .= "PlayerPosition=\"$position\"";
+                }
 
-    }
+            }
 
-    $search .= " ORDER BY  Rating DESC";
+            $search .= " ORDER BY  Rating DESC";
 
 
-    if ($stmt = $con->prepare($search)) {
-        $stmt->execute();
-        $stmt->bind_result($PlayerName, $PlayerPosition, $Province, $Rating);
-        echo "<table class=\"table table-hover\">
+            if ($stmt = $con->prepare($search)) {
+                $stmt->execute();
+                $stmt->bind_result($PlayerName, $PlayerPosition, $Province, $Rating);
+                echo "<table class=\"table table-hover\">
                 <thead>
                 <tr>
                     <th scope=\"col\">Name</th>
@@ -104,21 +107,25 @@ if(isset($_POST['submit'])){
                 </thead>
                 <tbody>";
 
-        while ($stmt->fetch()) {
-            echo "<tr>
+                while ($stmt->fetch()) {
+                    echo "<tr>
                     <td>$PlayerName</td>
                     <td>$PlayerPosition</td>
                     <td>$Province</td>
                     <td>$Rating</td>
                     <td><button type=\"button\" class=\"btn btn-success\">Buy Now</button></td>
                 </tr>";
-        }
-        echo "</tbody></table>";
-        $stmt->close();
-    }
-    else {
-        echo "<p>No results found.</p>";
-    }
-} ?>
+                }
+                echo "</tbody></table>";
+                $stmt->close();
+            }
+            else {
+                echo "<p>No results found.</p>";
+            }
+        } ?>
+    </div>
 </div>
+
+
+
 </body>
