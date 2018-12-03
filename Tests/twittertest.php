0d4fb0e8eb4dad8b646d8240bb6c9a51003dad26
David Sheehy <sheehy_d@ymail.com>
2018-12-03 13:29:13 +0000

    added first Twitter API files

---
+++ Tests/twittertest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Davy Sheehy
+ * Date: 03/12/2018
+ * Time: 13:09
+ */
+
+$consumerkey = "YcSTar2h6yKS8TVCW5wKq67cw";
+$consumersecret = "AxGPxUqd7NotcnKqWxq0njUrgfQ7SHI5clsTHkwpYp90zCwk7m";
+$accesstoken = "714884229298012161-Sfjiz78WlelGDogLngHuTHHxCspSEdB";
+$accesssecret = "uHVDgznCRmM6ynxwgfWxVwn3xVWiMJv0L8AnsXG6ziX08";
+
+require "../twitteroauth-0.9.2/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+//Connect API
+$connection = new TwitterOAuth($consumerkey,$consumersecret,$accesstoken,$accesssecret);
+$content = $connection->get("account/verify_credentials");
+
+//Create Tweet
+$new_status = $connection->post("statuses/update", ["status" => "Up Munster!"]);
+
+//Get Tweets
+$statuses = $connection->get("statuses/home_timeline",["count" => 1, "exclude_replies" => true]);
+
+print_r($statuses);
+
+?>
